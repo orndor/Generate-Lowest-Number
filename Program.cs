fabaffb5e888b430afec0ca3990328f53d4a0674
Rob Orndoff <rorndoff@gmail.com>
2019-08-28 19:38:03 -0400

    Commit with functional program.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Linq;
 
-namespace Generate_Lowest_Number
+namespace FindLowestNumber
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a list of numbers with no spaces in between: ");
+            var number = Console.ReadLine();
+
+            Console.Write("Enter the number of characters which should be removed: ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GenerateLowestNumber(number, n));
+        }
+        public static string GenerateLowestNumber(string number, int n)
+        {
+            var listOfInts = number.Select(numbers => int.Parse(numbers.ToString())).ToList();
+            var iterator = listOfInts.Count - n;
+            var outputString = "The lowest number which can be created is: ";
+
+            for (int i = 0; i < iterator; i++)
+            {
+                var tempList = listOfInts.Take(listOfInts.Count - (iterator - 1 - i));
+                var lowestUsableNum = tempList.Min();
+                var indexOfLowestUsableNum = listOfInts.IndexOf(tempList.Min());
+
+                listOfInts.RemoveRange(0, indexOfLowestUsableNum + 1);
+                outputString += lowestUsableNum;
+            }
+
+            return outputString;
         }
     }
 }
