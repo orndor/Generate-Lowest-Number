b611582cdaa998f77655774994004e352fd987a0
Rob Orndoff <rorndoff@gmail.com>
2019-08-28 19:28:02 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generate_Lowest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Generate-Lowest-Number
